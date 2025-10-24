d83986a359a040fa68cf4e48e8dcbfeed9f29e23
Gloria Maria Colmenares Montero <glo54486@limestone.hpc.ieg.fraunhofer.de>
2025-10-24 11:07:27 +0200

    to run the FCR feature

--- 01_Python_code/paths.py
+++ 01_Python_code/paths.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
-BASE_INPUT_PATH = Path(r"/ceph/home/ali97393/01_Working_package_1/02_Input_data")
-BASE_OUTPUT_PATH = Path(r"/ceph/home/ali97393/01_Working_package_1/03_Output")
+BASE_INPUT_PATH = Path(r"/ceph/home/glo54486/ceph/Economic_LSTM/02_Input_data")
+BASE_OUTPUT_PATH = Path(r"/ceph/home/glo54486/ceph/Economic_LSTM/03_Output")
+
