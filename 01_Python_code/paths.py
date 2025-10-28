b5bc7db71ca612e6fa723fc2f450d00e9a82ba72
Ali Naseri <1377alinaseri@gmail.com>
2025-10-28 17:42:56 +0100

    added fcr data as exogenous factor and removed paths.py from tracking

--- 01_Python_code/paths.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-BASE_INPUT_PATH = Path(r"/ceph/home/glo54486/ceph/Economic_LSTM/02_Input_data")
-BASE_OUTPUT_PATH = Path(r"/ceph/home/glo54486/ceph/Economic_LSTM/03_Output")
-
